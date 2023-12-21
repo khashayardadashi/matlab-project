e787db04318359c7eeb010e1b1edc6403ca1963d
khashayardadashi <115826321+khashayardadashi@users.noreply.github.com>
2023-12-21 20:56:57 +0330

    Update calculate_binomial_pmf_and_mean.m

--- binomial/calculate_binomial_pmf_and_mean.m
+++ binomial/calculate_binomial_pmf_and_mean.m
@@ -1,20 +1,29 @@
 function calculate_binomial_pmf_and_mean(n, p)
-
-    values_for_k = 0:n;
-    probabilities = zeros(1, n+1);
-
-    for k = values_for_k
-        probabilities(k + 1) = nchoosek(n, k) * p^k * (1 - p)^(n - k);
+     
+    results = zeros(1, 10000);
+    for i = 1:10000
+        results(i) = binomialDistribution(p, n);
     end
 
-    mean_val = sum(values_for_k .* probabilities);
+    Frequency = sum(results) / (n * 10000);
+    binomial_mean = n*p;
 
-    figure;
-    stem(values_for_k, probabilities, 'b' ,'filled');
+    x = 1:n;
+    pmf = hist(results, x) / 10000;
+    stem(x, pmf, 'LineWidth', 1.5);
+    title('Binomial Distribution Pmf');
     xlabel(['X ~ Binomial ( n = ' num2str(n) ', p = ' num2str(p) ')']);
     ylabel('P(X = x)');
-    title('Binomial Distribution Pmf');
     grid on;
 
-    disp(['Mean of the random variable X = ' num2str(mean_val)]);
+     disp(['Mean of the random variable X = ' num2str(binomial_mean)]);
+end
+
+function count = binomialDistribution(p, n)
+    count = 0;
+    for i = 1:n
+        if rand() < p
+            count = count + 1;
+        end
+    end
 end
