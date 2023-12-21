cf013b5d43b84783cc081c0f741839218cb31af2
khashayardadashi <115826321+khashayardadashi@users.noreply.github.com>
2023-12-21 20:56:13 +0330

    Update calculate_geometric_pmf_and_mean.m

--- geometric/calculate_geometric_pmf_and_mean.m
+++ geometric/calculate_geometric_pmf_and_mean.m
@@ -1,13 +1,26 @@
 function calculate_geometric_pmf_and_mean(n, p)
 
-    pmfs = (1-p).^(0:n-1) * p;
-    expected_value = sum(pmfs);
+    n = round(n);
+    results = zeros(1, n);
+
+    for i = 1:n
+        trialsUntilSuccess = 1;
+        while rand() > p
+            trialsUntilSuccess = trialsUntilSuccess + 1;
+        end
+        results(i) = trialsUntilSuccess;
+    end
+
+    % Plot the PMF
+    counts = hist(results, 1:max(results));
+    pmf = counts / n;
 
     figure;
-    stem(pmfs ,'filled');
+    stem(1:max(results), pmf, 'filled');
+    title('Geometric Distribution Pmf');
     xlabel(['X ~ Geometric(n=' num2str(n) ', p=' num2str(p) ')']);
     ylabel('P[X=x]');
-    title('Geometric Distribution Pmf');
-    disp(['Mean of the random variable X = ' num2str(expected_value)]);
 
+    meanValue = mean(results);
+    disp(['Mean of the random variable X = ' num2str(meanValue)]);
 end
